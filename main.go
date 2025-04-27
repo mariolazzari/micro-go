fce2f0ab9a5c9fb4d19f1f2d6756ddaff368b5fc
Mario Lazzari <mario.lazzari@gmail.com>
2025-04-27 09:38:30 +0200

    Done chapter 1

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/mariolazzari/micro-go/internal/database"
+	"github.com/mariolazzari/micro-go/internal/server"
+)
+
+func main() {
+	db, err := database.NewDatabaseClient()
+	if err != nil {
+		log.Fatalf("failed to initialize Database Client: %s", err)
+	}
+	srv := server.NewEchoServer(db)
+	if err := srv.Start(); err != nil {
+		log.Fatal(err.Error())
+	}
 }
