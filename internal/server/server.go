fb0c0b871bd1124fc7207c5079c58e70c6686828
Mario Lazzari <mario.lazzari@gmail.com>
2025-04-28 08:31:35 +0200

    Done chapter 2 lessson 7

--- internal/server/server.go
+++ internal/server/server.go
@@ -16,6 +16,7 @@ type Server interface {
 
 	GetAllCustomers(ctx echo.Context) error
 	AddCustomer(ctx echo.Context) error
+	GetCustomerById(ctx echo.Context) error
 
 	GetAllProducts(ctx echo.Context) error
 	AddProduct(ctx echo.Context) error
@@ -56,6 +57,7 @@ func (s *EchoServer) registerRoutes() {
 	cg := s.echo.Group("/customers")
 	cg.GET("", s.GetAllCustomers)
 	cg.POST("", s.AddCustomer)
+	cg.GET("/:id", s.GetCustomerById)
 
 	pg := s.echo.Group("/products")
 	pg.GET("", s.GetAllProducts)
